660cdef0b324633056a05886ca6bb91e9884feea
richard mai <richardmai@richardtekimacbook-air.local>
2016-04-12 21:17:45 +0800

    Initial Commit

---
+++ homework_02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  homework_02
+//
+//  Created by richard mai on 2016/4/12.
+//  Copyright © 2016年 richard mai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
